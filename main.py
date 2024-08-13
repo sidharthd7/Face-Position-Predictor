14b907f57c7d81dc7492120712f1179d116f64c8
Sidharth Dhawan <sidharthdhawan17@gmail.com>
2024-08-13 14:41:24 +0530

    Added .gitignore - Updated main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import os
 from PIL import Image
 from landingai.predict import Predictor
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Enter your API Key
 endpoint_id = "075ce9bb-529c-4d14-8b9b-68b84936e759"
-api_key = "land_sk_vF3XDTrVjFc9z1VKNaCppTzcgmZlDidQcQtdGRQSEqGVS8uCFz"
+api_key = "LANDING_AI_API_KEY"
 
 # Load your image
 image = Image.open("image.png")
